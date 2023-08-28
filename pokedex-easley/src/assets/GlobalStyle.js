6a6ad1dea03fd80ca99cabe58679212e284bcbb3
ericreisdev <ericreis.dev@gmail.com>
2023-08-28 17:00:25 -0300

    inicio do projeto, irei entregar no final do curso, mandando pra constar, pois peguei muitos freelances, e já ajeitei com a monitoria

---
+++ pokedex-easley/src/assets/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    background-color: white;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
+
+
+`
